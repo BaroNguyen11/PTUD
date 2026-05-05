21f9b15d5add128f2ed2467c83c4faaef941770a
Nguyen Vu Thanh <23667451.thanh@student.iuh.edu.vn>
2026-05-05 15:46:53 +0700

    Keep only src folder

--- tmp/MongoCollectionNames.java
+++
@@ -1,9 +0,0 @@
-import com.mongodb.client.*;
-public class MongoCollectionNames {
-  public static void main(String[] args) {
-    try (MongoClient client = MongoClients.create("mongodb://localhost:27017")) {
-      MongoDatabase db = client.getDatabase("QLNhaHang2BTCHECK");
-      for (String name : db.listCollectionNames()) System.out.println(name);
-    }
-  }
-}
